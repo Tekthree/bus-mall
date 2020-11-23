f798ef645ff35777c554944c932bf6ac1ccbc39d
tekthree <tekthree@gmail.com>
2020-11-23 12:27:42 -0800

    finished up lab11

--- JS/app.js
+++ JS/app.js
@@ -3,15 +3,16 @@
 var imagesArray = [];
 var randomNumberArray = [];
 var totalVotes = 0;
+// why cant I use getElementsByClassName....fustrating...spent about a hour troubleshooting 
 var imgWrap = document.getElementById('images-wrapper');
 var imagesOne = document.getElementById('one-images');
 var imagesTwo = document.getElementById('two-images');
 var imagesThree = document.getElementById('three-images');
+var resultsArea = document.getElementById('results');
 
 
 
 var Images = function(name,path='jpg'){
-  this.title = name;
   this.filepath = `./img/${name}.${path}`;
   this.title = name;
   this.alt = name;
@@ -38,7 +39,7 @@ new Images('pen');
 new Images('pet-sweep');
 new Images('scissors');
 new Images('shark');
-new Images('sweep');
+new Images('sweep','png');
 new Images('tauntaun');
 new Images('unicorn');
 new Images('usb','gif');
@@ -106,11 +107,12 @@ var renderImages = function (){
   makeUniqueRandomNums();
 
   
-
+  // assign variables for each random number in array
   var firstImages = randomNumberArray[0];
   var secondImages = randomNumberArray[1];
   var thirdImages = randomNumberArray[2];
 
+  // rewrite what images are in the html, with the random numbers in random num array
   imagesOne.src = imagesArray[firstImages].filepath;
   imagesOne.title = imagesArray[firstImages].title;
   imagesOne.alt = imagesArray[firstImages].alt;
@@ -126,15 +128,57 @@ var renderImages = function (){
   imagesThree.alt = imagesArray[thirdImages].alt;
   imagesArray[thirdImages].views++;
 
-  console.log(imagesArray[firstImages].filepath);
-  console.log(imagesOne.src);
+  console.log(imagesArray[firstImages].title);
+  console.log(imagesArray[secondImages].title);
+  console.log(imagesArray[thirdImages].title);
+  // console.log(imagesOne.src);
   
 }  
 
 
+// function for checking to see if targeted title matches on of the titles in the image array]
+// removing event listner
+function voteEvent(e){
+  var clickedOnTitle = e.target.title;
+
+  for(var i=0; i > imagesArray.length; i++){
+    if(clickedOnTitle === imagesArray[i].title){
+      imagesArray[i].votes++;
+      
+      
+      
+    }
+    
+  }
+
+  //Get article, create new p element, fill it with voted title and append to child
+  var resultsId = document.getElementById('article');
+  // console.log(resultsId);
+  var pElement = document.createElement('p');
+  pElement.innerText = `this item was voted on: ${clickedOnTitle}`;
+  resultsId.appendChild(pElement);
+  totalVotes++;
+
+  // console.log(totalVotes)
+  if(totalVotes === 25){
+    imgWrap.removeEventListener('click', voteEvent);
+  }
+
+  console.log(totalVotes);
+
+  document.getElementById('results').innerText = `the total votes are ${totalVotes}`;
 
 
-// imgWrap.addEventListener('click', voteImages);
+
+  // render more images to html
+  renderImages();
+
+}
+
+// Event listener for the Image Wrapper
+imgWrap.addEventListener('click', voteEvent)
+
+
 
 
 renderImages();
